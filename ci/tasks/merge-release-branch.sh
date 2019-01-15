eeee9c6bff42df680e9fa7f5cd24f4a792efa317
root <root@vmhost01>
2019-01-15 05:43:23 +0000

    update

--- ci/tasks/merge-release-branch.sh
+++ ci/tasks/merge-release-branch.sh
@@ -5,7 +5,5 @@ shopt -s dotglob
 mv -f ../repo/* ./
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
-#git remote add -f repo-stg ../repo-stg
-#git merge --no-edit repo-stg/repo-stg
-git checkout master
-git merge staging
+git remote add -f staging ../repo-stg
+git merge --no-edit staging/staging
